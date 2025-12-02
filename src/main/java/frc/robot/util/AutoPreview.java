743daef41afabb03b4a17ad2805a190917181826
Eddy Walsh <WalshEddy5@gmail.com>
2025-12-02 15:08:35 -0800

    Refactor to use scoring pose records

--- src/main/java/frc/robot/util/AutoPreview.java
+++ src/main/java/frc/robot/util/AutoPreview.java
@@ -1,48 +1,46 @@
 package frc.robot.util;
 
-import java.util.List;
-
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.pathplanner.lib.path.PathPlannerPath;
-
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.FieldObject2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj2.command.Command;
+import java.util.List;
 
 public class AutoPreview {
-    public static void initAutoPreview(SendableChooser<String> autoName, Field2d field) {
-        List<String> options = AutoBuilder.getAllAutoNames();
-        System.out.println("Available autos for preview: " + options);
-        autoName.onChange((newAuto) -> {
-            if (options.contains(newAuto)) {
-                System.out.println("Visualizing auto: " + newAuto);
-                visualizeAuto(newAuto, field);
-            } else {
-                clearField(field);
-            }
+  public static void initAutoPreview(SendableChooser<String> autoName, Field2d field) {
+    List<String> options = AutoBuilder.getAllAutoNames();
+    System.out.println("Available autos for preview: " + options);
+    autoName.onChange(
+        (newAuto) -> {
+          if (options.contains(newAuto)) {
+            System.out.println("Visualizing auto: " + newAuto);
+            visualizeAuto(newAuto, field);
+          } else {
+            clearField(field);
+          }
         });
-    }
+  }
 
-    private static void visualizeAuto(String autoName, Field2d field) {
-        List<PathPlannerPath> auto = null;
-        try {
-            auto = PathPlannerAuto.getPathGroupFromAutoFile(autoName);
-        } catch (Exception e) {
-            DriverStation.reportError("Auto file for visualization not found", e.getStackTrace());
-        }
-        if(auto != null) {
-            FieldObject2d fieldObj = field.getObject("Auto Preview");
-            for (PathPlannerPath path : auto) {
-                fieldObj.setPoses(path.getPathPoses());
-            }
-        }
+  private static void visualizeAuto(String autoName, Field2d field) {
+    List<PathPlannerPath> auto = null;
+    try {
+      auto = PathPlannerAuto.getPathGroupFromAutoFile(autoName);
+    } catch (Exception e) {
+      DriverStation.reportError("Auto file for visualization not found", e.getStackTrace());
     }
+    if (auto != null) {
+      FieldObject2d fieldObj = field.getObject("Auto Preview");
+      for (PathPlannerPath path : auto) {
+        fieldObj.setPoses(path.getPathPoses());
+      }
+    }
+  }
 
-    private static void clearField(Field2d field) {
-        FieldObject2d fieldObj = field.getObject("Auto Preview");
-        fieldObj.setPoses(List.of());
-    }
+  private static void clearField(Field2d field) {
+    FieldObject2d fieldObj = field.getObject("Auto Preview");
+    fieldObj.setPoses(List.of());
+  }
 }
